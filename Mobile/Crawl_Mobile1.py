2d4c63e329301541ff380e0b119a0318f37ca2ab
Jinuk <sksmsx147@gmail.com>
2020-06-11 12:50:58 +0900

    Crawl_mobile incomplete

--- Mobile/Crawl_Mobile1.py
+++ Mobile/Crawl_Mobile1.py
@@ -9,10 +9,11 @@ class Crawler():
         self.pname = ['Alarm.py','Game.py','Music.py','Note.py','Training.py','Video.py']
         self.pflag = dict(zip((self.pname),[0,0,0,0,0,0]))
         self.pid = dict(zip((self.pname),[0,0,0,0,0,0]))
-        self.index = 1
+        self.index = 0
 
     def Crawling(self):
         threading.Timer(1,self.Crawling).start()
+        self.Write(self.pflag.values)
         process = psutil.pids()
         for p in process:
             try:
@@ -22,23 +23,26 @@ class Crawler():
                         if(self.pid[i] not in process):
                             self.pflag[i] = 0
                             self.pid[i] = 0
-                            self.Write(i, 0)
                             continue
                             
                     if(pro.name() == "python.exe" and i == pro.cmdline()[1] and not self.pflag[i]):
                         self.pflag[i]=1
                         self.pid[i]=pro.pid
-                        self.Write(i, 1)
             except:
                 continue
 
-    def Write(self,name,flag):
+    def Write(self,flag):
         f = open('mobile_data.csv','a',encoding = 'utf-8', newline ='')
         wr = csv.writer(f)
         tm = time.localtime(time.time())
-        wr.writerow([self.index, name, time.strftime('%c', tm),"{0:02d}:{1:02d}:{2:02d}".format(tm.tm_hour,tm.tm_min,tm.tm_sec), flag])
+        wr.writerow([self.index, "{0:02d}/{1:02d} {2:02d}:{3:02d}:{4:02d}".format(tm.tm_mon, tm.tm_mday, tm.tm_hour,tm.tm_min,tm.tm_sec)] + flag)
+        f.close()
         self.index += 1
 
 if(__name__ == "__main__"):
+    f = open('mobile_data.csv','w',encoding = 'utf-8', newline ='')
+    wr = csv.writer(f)
+    wr.writerow(["INDEX", "TIME", "Alarm", "Game", "Music", "Note", "Training", "Video"])
+    f.close()
     mobile = Crawler()
     mobile.Crawling()
