e9dc977f6ba4d8e43b63b2ff768dcf2c4f3673ca
Jinuk <sksmsx147@gmail.com>
2020-06-12 02:14:19 +0900

    Multiprocessing

--- Mobile/Crawl_Mobile1.py
+++ Mobile/Crawl_Mobile1.py
@@ -2,8 +2,24 @@
 import psutil
 import time
 import csv
-import threading
+from multiprocessing import Process, Event
 import os
+
+class Timer(Process):
+    def __init__(self, interval, function, args=[], kwargs={}):
+        super(Timer, self).__init__()
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.finished = Event()
+
+    def run(self):
+        self.finished.wait(self.interval)
+        if not self.finished.is_set():
+            self.function(*self.args, **self.kwargs)
+        self.finished.set()
+
 
 class Crawler():
     def __init__(self):
@@ -13,8 +29,7 @@ class Crawler():
         self.index = 0
 
     def Crawling(self):
-        print(".",end="")
-        threading.Timer(1.0,self.Crawling).start()
+        Timer(2,self.Crawling).start()
         self.Write(list(self.pflag.values()))
         process = psutil.pids()
         for p in process:
