88f85d70320e176d1006ca6af98f46f9d1618cf5
Jinuk <sksmsx147@gmail.com>
2020-06-11 18:55:13 +0900

    Auto Crawling Process Complete

--- Mobile/Crawl_Mobile1.py
+++ Mobile/Crawl_Mobile1.py
@@ -3,17 +3,18 @@ import psutil
 import time
 import csv
 import threading
+import os
 
 class Crawler():
     def __init__(self):
-        self.pname = ['Alarm.py','Game.py','Music.py','Note.py','Training.py','Video.py']
-        self.pflag = dict(zip((self.pname),[0,0,0,0,0,0]))
-        self.pid = dict(zip((self.pname),[0,0,0,0,0,0]))
+        self.pname = [os.getcwd()+'\\Controller_Mobile.py','Alarm.py','Game.py','Music.py','Note.py','Training.py','Video.py']
+        self.pflag = dict(zip((self.pname),[0,0,0,0,0,0,0]))
+        self.pid = dict(zip((self.pname),[0,0,0,0,0,0,0]))
         self.index = 0
 
     def Crawling(self):
-        threading.Timer(1,self.Crawling).start()
-        self.Write(self.pflag.values)
+        threading.Timer(2,self.Crawling).start()
+        self.Write(list(self.pflag.values()))
         process = psutil.pids()
         for p in process:
             try:
@@ -35,14 +36,14 @@ class Crawler():
         f = open('mobile_data.csv','a',encoding = 'utf-8', newline ='')
         wr = csv.writer(f)
         tm = time.localtime(time.time())
-        wr.writerow([self.index, "{0:02d}/{1:02d} {2:02d}:{3:02d}:{4:02d}".format(tm.tm_mon, tm.tm_mday, tm.tm_hour,tm.tm_min,tm.tm_sec)] + flag)
+        wr.writerow([self.index, "{0:04d}/{1:02d}/{2:02d}".format(tm.tm_year,tm.tm_mon, tm.tm_mday), "{0:02d}:{1:02d}:{2:02d}".format(tm.tm_hour,tm.tm_min,tm.tm_sec)] + flag)
         f.close()
         self.index += 1
 
 if(__name__ == "__main__"):
     f = open('mobile_data.csv','w',encoding = 'utf-8', newline ='')
     wr = csv.writer(f)
-    wr.writerow(["INDEX", "TIME", "Alarm", "Game", "Music", "Note", "Training", "Video"])
+    wr.writerow(["INDEX", "DATE", "TIME", "Controller", "Alarm", "Game", "Music", "Note", "Training", "Video"])
     f.close()
     mobile = Crawler()
     mobile.Crawling()
