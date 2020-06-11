57171c3a3a705751456004fd481b486c09a07278
Jinuk <sksmsx147@gmail.com>
2020-06-12 02:16:37 +0900

    threading method implementation

--- Mobile/Crawl_Mobile1.py
+++ Mobile/Crawl_Mobile1.py
@@ -2,23 +2,9 @@
 import psutil
 import time
 import csv
-from multiprocessing import Process, Event
+import threading
 import os
 
-class Timer(Process):
-    def __init__(self, interval, function, args=[], kwargs={}):
-        super(Timer, self).__init__()
-        self.interval = interval
-        self.function = function
-        self.args = args
-        self.kwargs = kwargs
-        self.finished = Event()
-
-    def run(self):
-        self.finished.wait(self.interval)
-        if not self.finished.is_set():
-            self.function(*self.args, **self.kwargs)
-        self.finished.set()
 
 
 class Crawler():
@@ -29,7 +15,7 @@ class Crawler():
         self.index = 0
 
     def Crawling(self):
-        Timer(2,self.Crawling).start()
+        threading.Timer(2,self.Crawling).start()
         self.Write(list(self.pflag.values()))
         process = psutil.pids()
         for p in process:
