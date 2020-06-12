ca8314dee0ccdc776a20a44ebafadba857b68f7f
Jinuk <sksmsx147@gmail.com>
2020-06-12 18:37:50 +0900

    Crawler output timeinterval

--- Mobile/Crawl_Mobile1.py
+++ Mobile/Crawl_Mobile1.py
@@ -1,10 +1,11 @@
-# 모바일 기기에서 프로세스 크롤링
+# 모바일 기기에서 프로세스 크롤링, 꾸준히 크롤링
 import psutil
 import time
 import csv
 import threading
 import os
-
+import copy
+import datetime
 
 
 class Crawler():
@@ -15,7 +16,7 @@ class Crawler():
         self.index = 0
 
     def Crawling(self):
-        threading.Timer(2,self.Crawling).start()
+        threading.Timer(5,self.Crawling).start()
         self.Write(list(self.pflag.values()))
         process = psutil.pids()
         for p in process:
@@ -37,15 +38,18 @@ class Crawler():
     def Write(self,flag):
         f = open('mobile_data.csv','a',encoding = 'utf-8', newline ='')
         wr = csv.writer(f)
-        tm = time.localtime(time.time())
-        wr.writerow([self.index, "{0:04d}/{1:02d}/{2:02d}".format(tm.tm_year,tm.tm_mon, tm.tm_mday), "{0:02d}:{1:02d}:{2:02d}".format(tm.tm_hour,tm.tm_min,tm.tm_sec)] + flag)
+        tm = datetime.datetime.now()
+        tmnow = datetime.datetime.now().time()
+        tminterval = tmnow.hour * 3600 + tmnow.minute * 60 + tmnow.second
+        wr.writerow([self.index, "{0:04d}/{1:02d}/{2:02d}".format(tm.year,tm.month, tm.day),
+        "{0:02d}:{1:02d}:{2:02d}".format(tm.hour,tm.minute,tm.second), tminterval] + flag)
         f.close()
         self.index += 1
 
 if(__name__ == "__main__"):
     f = open('mobile_data.csv','w',encoding = 'utf-8', newline ='')
     wr = csv.writer(f)
-    wr.writerow(["INDEX", "DATE", "TIME", "Controller", "Alarm", "Game", "Music", "Note", "Training", "Video"])
+    wr.writerow(["INDEX", "DATE", "TIME", "Interval", "Controller", "Alarm", "Game", "Music", "Note", "Training", "Video"])
     f.close()
     print("Process Crawler")
     print("If you shut down, press Ctrl + c")
